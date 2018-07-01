725bdf54eaafe534a6ebd9b572aa5e7f26daf6a0
jaikrat_tariyal <jaikrat.singh@gmail.com>
2018-07-01 08:30:43 +0530

    change in interface

--- src/com/tektutorial/funcinterface/Square.java
+++ src/com/tektutorial/funcinterface/Square.java
@@ -5,13 +5,4 @@ public interface Square {
 
 	int doSquare(int number);	
 	
-	public default void toStringe() {
-		System.out.println("hello");
-	}
-	
-	public static void toStringf() {
-		System.out.println("hello");
-	}
-	
-	abstract String toString() ;
 }
